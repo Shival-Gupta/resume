5d936fe2e0be965de1596096fa9ac17212dad47c
Shival Gupta <sgupta.5545@gmail.com>
2023-10-19 01:33:44 +0530

    added promise based async download function

--- scripts.js
+++ scripts.js
@@ -10,7 +10,39 @@ function showButtons() {
     document.getElementById('print-button').style.display = 'inline-block';
 }
 
-// Function to handle download
+// Function to handle print
+function printFunction() {
+    hideButtons();
+    window.print();
+    showButtons();
+}
+
+// Function to handle download (New Promise-based)
+async function downloadFunction() {
+    hideButtons();
+    try {
+        const element = document.getElementById('body');
+        const formattedDate = new Date().toLocaleDateString('en-GB').replace(/\//g, '-');
+        const options = {
+            margin: 10,
+            filename: `Shival_Gupta_Resume_${formattedDate}.pdf`,
+            image: { type: 'jpeg', quality: 1.0 },
+            html2canvas: { scale: 4 },
+            jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
+        };
+        // Create an HTML2PDF instance with the source element and options
+        const pdf = new html2pdf().from(element).set(options);
+        await pdf.outputPdf().save();   // Generate and save the PDF
+
+    } catch (error) {
+        console.error('PDF generation error:', error);
+    } finally {
+        showButtons();
+    }
+}
+
+// Function to handle download (Old Monolythic-style)
+/*
 function downloadFunction() {
     hideButtons();
     const element = document.getElementById('body');
@@ -25,10 +57,4 @@ function downloadFunction() {
     html2pdf(element, options);
     showButtons();
 }
-
-// Function to handle print
-function printFunction() {
-    hideButtons();
-    window.print();
-    showButtons();
-}
+*/
