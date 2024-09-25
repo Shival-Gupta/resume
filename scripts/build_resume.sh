a4101cffbec30eddef6e451869d0ddc33cd709f2
Shival Gupta <sgupta.5545@gmail.com>
2024-09-26 01:17:40 +0530

    Update Resume

--- scripts/build_resume.sh
+++ scripts/build_resume.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
-your_name="ShivalGupta" 
+name="ShivalGupta"
 
-# Compile LaTeX to PDF with your name in the filename
-xelatex -output-directory=build/ -jobname="RESUME_${your_name}" RESUME.tex 
+# Check if xelatex is available
+if ! command -v xelatex &> /dev/null; then
+    echo "Error: xelatex command not found. Please make sure it's installed."
+    exit 1  # Indicate failure
+fi
+
+# Compile LaTeX to PDF directly in the root directory
+xelatex_command="xelatex -jobname=\"RESUME_${name}\" RESUME.tex"
+
+# Execute the xelatex command and capture its output
+output=$(eval "$xelatex_command" 2>&1)
+
+# Check the exit status of the xelatex command
+if [ $? -ne 0 ]; then
+    echo "Error compiling LaTeX:"
+    echo "$output"
+    exit 1  # Indicate failure
+fi
 
 # Update README.md (replace placeholders with actual values)
-sed -i "s/RESUME_NAME_DATE\.pdf/RESUME_${your_name}\.pdf/g" README.md
+sed -i "s/RESUME_NAME_DATE\.pdf/RESUME_${name}\.pdf/g" README.md
 
 echo "Build complete!"
